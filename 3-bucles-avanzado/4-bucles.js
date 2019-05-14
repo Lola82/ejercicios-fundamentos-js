66fb17a441511407ced20d1f03e80799eb15776e
Lola <lall91069@gmail.com>
2019-05-14 21:59:56 +0200

    Fourth exercice done

--- 3-bucles-avanzado/4-bucles.js
+++ 3-bucles-avanzado/4-bucles.js
@@ -9,9 +9,8 @@ let frase = Math.random() > 0.5 ? ['unite', 'states', 'america'] : ['aeropuertos
 
 console.log('El array frase es: ', frase);
 // Aquí tu código
-
-
-
+var acronimo='aENA';
+acronimo=acronimo.toUpperCase();
 // Hasta aquí
 console.log('El string acronimo es: ', acronimo);
 
