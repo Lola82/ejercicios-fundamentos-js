e70b722ae2e0b4f49ddd5ebc51c625da95d3a1dc
Lola <lall91069@gmail.com>
2019-05-14 16:17:09 +0200

    Ninth exercice done

--- 1-bucles/9-array-bidimensional.js
+++ 1-bucles/9-array-bidimensional.js
@@ -6,9 +6,14 @@
 //   ['10','11','12'],
 //   ['20','21','22']
 // ];
-
 let matriz = [
     [],
     [],
     []
 ]
+for(var i=0; i<3;i++){
+    for( var j=0; j<3;j++){
+        matriz[i].push(i + "" + j);
+    }
+}
+console.log(matriz);
